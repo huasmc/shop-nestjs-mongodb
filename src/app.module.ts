b9daecd1788c4bf0515ca860e98d287813e6233d
huasmc <huasmc@gmail.com>
2022-12-23 17:21:55 -0400

    feat: users schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.ATLAS_URI),
     ConfigModule.forRoot(),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
